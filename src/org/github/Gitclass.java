dc4794a13e144dcefbce4e91e953a744f9fc739d
meenuanusiya <meenuanusiya1999@gmail.com>
2022-01-31 12:49:39 +0530

    M2 done Her coding

--- src/org/github/Gitclass.java
+++ src/org/github/Gitclass.java
@@ -1,7 +1,24 @@
 package org.github;
 
 public class Gitclass {
-	public static void main(String[] args) {
-		System.out.println("Anusiya");
+	private void master() {
+		System.out.println("Master Coding");
 	}
+
+	private void m1() {
+		System.out.println("M1 writting her coding");
+	}
+
+	private void m2() {
+		System.out.println("Member 2 coding");
+	}
+
+	public static void main(String[] args) {
+		Gitclass c = new Gitclass();
+		c.master();
+		c.m1();
+
+		c.m2();
+	}
+
 }
